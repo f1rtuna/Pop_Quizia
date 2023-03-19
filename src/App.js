db38171ea754785bac92ecbc65cb380f9115d8a1
Kil-Hwan <kilhwanpyun@gmail.com>
2023-03-19 04:21:04 -0400

    added error handling for buttons

--- src/App.js
+++ src/App.js
@@ -5,23 +5,35 @@ import HomePage from "./components/HomePage"
 import axios from 'axios'
 import { nanoid } from "nanoid";
 import './App.css';
+import { setSelectionRange } from "@testing-library/user-event/dist/utils"
 // https://opentdb.com/api.php?amount=10
 
 function App() {
-  const length = 10;
+  const length = 10
   const [started, setStarted] = useState(false)
   const [loading, setLoading] = useState(true)
   const [quizLink, setQuizLink] = useState(`https://opentdb.com/api.php?amount=${length}`)
   const [questions, setQuestions] = useState([])
   const [currentIndex, setCurrentIndex] = useState(0)
   const [score, setScore] = useState(0)
+  const [showAnswers, setShowAnswers] = useState(false)
+  const [selected0, setSelected0] = useState("choice")
+  const [selected1, setSelected1] = useState("choice")
+  const [selected2, setSelected2] = useState("choice")
+  const [selected3, setSelected3] = useState("choice")
+
 
   useEffect(() =>{
     setLoading(false)
     axios.get(quizLink)
       .then(res => res.data)
       .then(data => {
-        setQuestions(data.results)
+          const questions = data.results.map((question) => ({
+            ...question,
+            answers:[question.correct_answer, ...question.incorrect_answers].sort(() => Math.random() - 0.5),
+            correctAnswer: question.correct_answer
+          }))
+        setQuestions(questions)
       });
   },[])
 
@@ -32,27 +44,71 @@ function App() {
     setStarted(true)
   }
 
-  function checkAnswer(response){
-    if (response === questions[currentIndex].correct_answer){
-      console.log("Correct!")
-      setScore(prev => prev + 1)
-      // setCurrentIndex(prev => prev + 1)
-    } else{
-      console.log("nope!")
+  function checkAnswer(response, idx){
+    if(!showAnswers){
+      if (response === questions[currentIndex].correct_answer){
+        setScore(prev => prev + 1)
+        // setCurrentIndex(prev => prev + 1)
+        if (idx === 0){
+          setSelected0("correctChoice")
+        }
+        else if (idx === 1){
+          setSelected1("correctChoice")
+        }
+        else if (idx === 2){
+          setSelected2("correctChoice")
+        }
+        else if (idx === 3){
+          setSelected3("correctChoice")
+        }
+      }
+      else{
+        if (idx === 0){
+          setSelected0("incorrectChoice")
+        }
+        else if (idx === 1){
+          setSelected1("incorrectChoice")
+        }
+        else if (idx === 2){
+          setSelected2("incorrectChoice")
+        }
+        else if (idx === 3){
+          setSelected3("incorrectChoice")
+        }
+      }
     }
-    setCurrentIndex(prevIndex => prevIndex + 1)
+    setShowAnswers(true)
   }
+
+  function handleNextQuestion(){
+    setCurrentIndex(prev => prev + 1)
+    setShowAnswers(false)
+    setSelected0("choice")
+    setSelected1("choice")
+    setSelected2("choice")
+    setSelected3("choice")
+  }
+
 
   if (loading) return <Loading />
   return (
     <div className = "quiz-container">
-      {started ? (currentIndex < questions.length ? <QuizPage
+      {started ? (currentIndex < questions.length ? 
+                <QuizPage
                   id = {nanoid()}
                   checkAnswer = {checkAnswer}
+                  handleNextQuestion = {handleNextQuestion}
                   question = {questions[currentIndex]}
-                  /> : <div>
+                  showAnswers = {showAnswers}
+                  selected0 = {selected0}
+                  selected1 = {selected1}
+                  show4 = {questions[currentIndex].answers.length>2}
+                  selected2 = {selected2}
+                  selected3 = {selected3}
+                /> : (<div className = "gameOver">
                       {`Congrats you finished the Quiz! you scored: ${score}`}
-                    </div>)
+                      <button>Try Again!</button>
+                    </div>))
                : <HomePage 
                   // started = {started}
                   start_game = {start_game}
--- src/components/QuizPage.js
+++ src/components/QuizPage.js
@@ -1,24 +1,20 @@
-export default function QuizPage({question, checkAnswer}){
-  function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
+import { all } from "axios"
+import React, {useState} from "react"
+
+export default function QuizPage({question, checkAnswer, handleNextQuestion, showAnswers, selected0, selected1, selected2, selected3, handleSelected, show4}){
   
-    // While there remain elements to shuffle.
-    while (currentIndex != 0) {
-  
-      // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-  
-      return array;
-  }
-  let allAnswers = question.incorrect_answers
-  allAnswers.push(question.correct_answer)
-  allAnswers = shuffle(allAnswers)
+  let allAnswers = question.answers
+
+  // {allAnswers.map((answer,idx) => {
+  //   const specialClassName = showAnswers ? (
+  //       answer === question.correct_answer ? "green-button": "red-button"
+  //   ) : "";
+  //   return(
+  //       <button className={`normal-button ${specialClassName}`} 
+  //       onClick = {() => checkAnswer(answer)}
+  //       dangerouslySetInnerHTML={{__html:answer}} />
+  //   )
+  // })}
 
   return (
     <div className="question-container">
@@ -26,10 +22,19 @@ export default function QuizPage({question, checkAnswer}){
         <h1 dangerouslySetInnerHTML={{__html:question.question}}></h1>
       </div>
       <div className="choicesList">
-        <button className = "choice" onClick = {() => checkAnswer(allAnswers[0])} dangerouslySetInnerHTML={{__html:allAnswers[0]}}></button>
-        <button className = "choice" onClick = {() => checkAnswer(allAnswers[1])} dangerouslySetInnerHTML={{__html:allAnswers[1]}}></button>
-        <button className = "choice" onClick = {() => checkAnswer(allAnswers[2])} dangerouslySetInnerHTML={{__html:allAnswers[2]}}></button>
-        <button className = "choice" onClick = {() => checkAnswer(allAnswers[3])} dangerouslySetInnerHTML={{__html:allAnswers[3]}}></button>
+        
+        <button className = {selected0} onClick = {() => {checkAnswer(allAnswers[0], 0)}} dangerouslySetInnerHTML={{__html:allAnswers[0]}}></button>
+        <button className = {selected1} onClick = {() => {checkAnswer(allAnswers[1], 1)}} dangerouslySetInnerHTML={{__html:allAnswers[1]}}></button>
+        {show4 &&
+          <>
+            <button className = {selected2} onClick = {() => {checkAnswer(allAnswers[2], 2)}} dangerouslySetInnerHTML={{__html:allAnswers[2]}}></button>
+            <button className = {selected3} onClick = {() => {checkAnswer(allAnswers[3], 3)}} dangerouslySetInnerHTML={{__html:allAnswers[3]}}></button>
+          </>
+        }
+        
+      </div>
+      <div className = "nextQuestion">
+        {showAnswers && <button onClick={handleNextQuestion}>Next Question</button>}
       </div>
     </div>
   )
